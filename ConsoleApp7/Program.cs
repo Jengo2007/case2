90c2b6f29e1adbefce5d6da8623d917d138b6ff9
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-27 16:52:54 +0300

    case2

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var day =Int32.Parse(Console.ReadLine());
+
+var month =Int32.Parse(Console.ReadLine());
+int daysInMonth = 0;
+switch (month)
+{
+    case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+        daysInMonth = 31;break;
+    case 4:case 6:case 9:case 11:
+        daysInMonth = 30;break;
+    case 2:
+        daysInMonth = 28;break;
+        default:
+            Console.WriteLine("Invalid month");
+            return;
+    
+}
+
+if (day < daysInMonth)
+{
+    day += 1;
+}
+else
+{
+    day = 1;
+    month += 1;
+    if (month > 12)
+    {
+        month = 1;
+    }        
+}
+Console.WriteLine($"Следуйший день{day +"-"+month}");
+        
+    
